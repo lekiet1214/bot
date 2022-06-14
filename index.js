b06b8194af20a93948fe776f4b3807b10133b03d
lekiet1214 <sisu1214@gmail.com>
2022-06-14 19:00:42 +0700

    test config

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const {
     ShardingManager
 } = require('discord.js');
 require('dotenv').config();
+const testShardList = []
+testShardList.push(1306)
 const manager = new ShardingManager('./bot.js', {
     token: process.env.TOKEN,
-	totalShards:'auto'
+    totalShards: 'auto',
+    shardList: [1306, 810],
+    respawn: 'true',
 });
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
 manager.spawn()
-	.then(shards => {
-		shards.forEach(shard => {
-			shard.on('message', message => {
-				console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
-			});
-		});
-	})
-	.catch(console.error);
+    .then(shards => {
+        shards.forEach(shard => {
+            shard.on('message', message => {
+                console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
+            });
+        });
+    })
+    .catch(console.error);
+
+manager.on('shardCreate', shard => {
+    shard.on("ready", () => {
+        console.log(`[DEBUG/SHARD] Shard ${shard.id} connected to Discord's Gateway.`)
+        // Sending the data to the shard.
+        shard.send({type: "shardId", data: {shardId: shard.id}});
+    });
+});
