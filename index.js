8010b3cf2bb7969baf018e6ad3344f026aeb23e8
lekiet1214 <sisu1214@gmail.com>
2022-06-14 11:53:19 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const {
 } = require('discord.js');
 require('dotenv').config();
 const manager = new ShardingManager('./bot.js', {
-    token: process.env.TOKEN
+    token: process.env.TOKEN,
+	totalShards:'auto'
 });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
