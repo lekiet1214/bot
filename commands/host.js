66b75314b3d2dc033febd93356dfbcdc8598cc52
lekiet1214 <sisu1214@gmail.com>
2022-06-14 21:09:06 +0700

    host stat command

--- commands/host.js
+++ commands/host.js
@@ -18,18 +18,34 @@ module.exports = {
         const cpustat = 'undefined'
         console.log(os.cpus)
         const timenow = Date.now();
-        const repli = new MessageEmbed()
-            .setColor('LUMINOUS_VIVID_PINK')
-            .setTitle('Host stats')
-            .setURL('https://discord.com')
-            .setDescription('Host statistic for nhobot')
-            .addField(
-                { name: 'CPU', value: 'undefined', inline: true },
-                { name: 'RAM', value: ramstat, inline: true },
-                { name: 'OS', value: operatingsys, inline: true },
-            )
-            .setTimestamp()
-            .setFooter({ text: 'nhobot by nho', iconURL: 'https://scontent.fsgn2-5.fna.fbcdn.net/v/t1.6435-9/120544636_667386287527542_7324991427113116012_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Iq3_chh4co0AX_fV_M2&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT9oX0pbkyoPItu1l-hJfHGFy0LTpt6RTQCBXSx8k5Qqng&oe=62CCD051' });
+        const repli = {
+            "color": 14637275,
+            "author": {
+              "name": "Host stat",
+              "url": "https://www.facebook.com/le.kiet1214/",
+              "icon_url": "https://scontent.fsgn2-5.fna.fbcdn.net/v/t1.6435-9/120544636_667386287527542_7324991427113116012_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Iq3_chh4co0AX_fV_M2&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT9oX0pbkyoPItu1l-hJfHGFy0LTpt6RTQCBXSx8k5Qqng&oe=62CCD051"
+            },
+            "title": "Host stat",
+            "url": "https://dicord.com",
+            "description": "Host stat for nhobot",
+            "fields": [
+              {
+                "name": "CPU",
+                "value": cpustat,
+                "inline": true
+              },
+              {
+                "name": "RAM",
+                "value": ramstat,
+                "inline": true
+              },
+              {
+                "name": "OS",
+                "value": operatingsys,
+                "inline": true
+              }
+            ]
+          }
         return await interaction.reply({ embeds: [repli] });
 
     },
