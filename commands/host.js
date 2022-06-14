c8bd3ef7160e9b8c07516988481f018f90bf2c11
lekiet1214 <sisu1214@gmail.com>
2022-06-14 21:24:53 +0700

    host command

--- commands/host.js
+++ commands/host.js
@@ -6,46 +6,48 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS]
 })
 const os = require('node:os')
-
+const fs = require('node:fs')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('host')
         .setDescription(`Replies with host's stats!`),
     async execute(interaction) {
         const operatingsys = os.platform + os.release;
-        const ramstat = (os.freemem / 1024) / 1024 + '/' + (os.totalmem / 1024) / 1024;
+        const ramstat = Math.floor((os.freemem / 1024) / 1024 /1024) + 'GB free out of ' + Math.floor((os.totalmem / 1024) / 1024/1024) + ' GB';
         // const cpustat = os.cpus[0].model;
-        const cpustat = 'undefined'
-        console.log(os.cpus)
+        const cpustat = os.cpus()[0].model;
+        fs.writeFile('host.log', JSON.stringify(os.cpus()[0].model, null, 2), 'utf-8', e => {
+            if (e) throw e;
+        })
         const timenow = Date.now();
         const repli = {
             "color": 14637275,
             "author": {
-              "name": "Host stat",
-              "url": "https://www.facebook.com/le.kiet1214/",
-              "icon_url": "https://scontent.fsgn2-5.fna.fbcdn.net/v/t1.6435-9/120544636_667386287527542_7324991427113116012_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Iq3_chh4co0AX_fV_M2&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT9oX0pbkyoPItu1l-hJfHGFy0LTpt6RTQCBXSx8k5Qqng&oe=62CCD051"
+                "name": "Host stat",
+                "url": "https://www.facebook.com/le.kiet1214/",
+                "icon_url": "https://scontent.fsgn2-5.fna.fbcdn.net/v/t1.6435-9/120544636_667386287527542_7324991427113116012_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Iq3_chh4co0AX_fV_M2&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT9oX0pbkyoPItu1l-hJfHGFy0LTpt6RTQCBXSx8k5Qqng&oe=62CCD051"
             },
             "title": "Host stat",
             "url": "https://dicord.com",
             "description": "Host stat for nhobot",
             "fields": [
-              {
-                "name": "CPU",
-                "value": cpustat,
-                "inline": true
-              },
-              {
-                "name": "RAM",
-                "value": ramstat,
-                "inline": true
-              },
-              {
-                "name": "OS",
-                "value": operatingsys,
-                "inline": true
-              }
+                {
+                    "name": "CPU",
+                    "value": cpustat,
+                    "inline": true
+                },
+                {
+                    "name": "RAM",
+                    "value": ramstat,
+                    "inline": true
+                },
+                {
+                    "name": "OS",
+                    "value": operatingsys,
+                    "inline": true
+                }
             ]
-          }
+        }
         return await interaction.reply({ embeds: [repli] });
 
     },
