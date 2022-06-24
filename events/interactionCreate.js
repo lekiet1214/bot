80bb4338a470385cc0160eba3106701b83d4b89d
lekiet1214 <sisu1214@gmail.com>
2022-06-25 00:11:31 +0700

    fresh start

--- events/interactionCreate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'interactionCreate',
-    on: true,
-    execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.commandName}.`);
-    },
-};
