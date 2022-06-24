f542bfb32ccbb517532ac1a5718ecf14511d23d6
lekiet1214 <sisu1214@gmail.com>
2022-06-24 10:27:42 +0700

    error debugging

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -17,7 +17,7 @@ const client = new Client({
     ]
 });
 const { Player } = require('discord-music-player');
-const fs = require('fs');
+// const fs = require('fs');
 
 client.player = new Player(client, {
     leaveOnEmpty: false,
@@ -57,205 +57,210 @@ module.exports = {
     name: 'messageCreate',
     on: true,
     async execute(message) {
-        if (message.author.bot) return;
+        try {
+            if (message.author.bot) return;
 
-        // DEBUG
-        // console.debug(client.guilds.resolve(message.guild.id))
-        // fs.writeFile('./message.debug.log', JSON.stringify(client.guilds.resolve(message.guild.id), null, 2), (err) => {
-        //     if (err) message.channel.send(err);
-        // }
-        // )
-        if (message.content.startsWith(process.env.PREFIX)) {
-            let song;
-            const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-            const command = args.shift().toLowerCase();
-            let guildQueue = client.player.getQueue(message.guild.id);
-            let queue = client.player.getQueue(message.guild.id);
-            if(guildQueue === undefined && queue === undefined) {
-                queue = client.player.createQueue(message.guild.id);
-                guildQueue = queue;
-            }
-            // Pre handling command args
-            switch (command) {
-                case 'play':
-                case 'playlist':
-                    if (args.length === 0) {
-                        message.channel.send('Please provide a link or a search query.');
-                        return;
-                    }
-                    break;
-            }
-            switch (command) {
-                case 'play':
-                    await queue.join(message.member.voice.channel);
-                    song = await queue.play(args.join(' ')).catch(_ => {
-                        if (!guildQueue)
-                            queue.stop();
-                    });
-                    break;
-                case 'skip':
-                    if (guildQueue) {
-                        guildQueue.skip();
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'stop':
-                    if (guildQueue) {
-                        guildQueue.stop();
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'pause':
-                    if (guildQueue) {
-                        guildQueue.setPaused(true);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'resume':
-                    if (guildQueue) {
-                        guildQueue.setPaused(false);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'volume':
-                    if (guildQueue) {
-                        message.channel.send(`Volume is ${guildQueue.volume}`);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'queue':
-                    if (guildQueue) {
-                        message.channel.send(guildQueue);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'repeat':
-                    if (guildQueue) {
-                        guildQueue.setRepeatMode(1);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'norepeat':
-                    if (guildQueue) {
-                        guildQueue.setRepeatMode(0);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'shuffle':
-                    if (guildQueue) {
-                        guildQueue.Shuffle();
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'queueloop':
-                    if (guildQueue) {
-                        guildQueue.setRepeatMode(2);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'setvolume':
-                    if (guildQueue) {
-                        guildQueue.setVolume(parseInt(args[0]));
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'np':
-                    if (guildQueue) {
-                        message.channel.send(`Now playing: ${guildQueue.nowPlaying}`);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'help':
-                    message.channel.send({ embeds: [helpMessage] });
-                    break;
-                case 'progress':
-                    if (guildQueue) {
-                        message.channel.send(`Progress: ${guildQueue.createProgressBar().prettier()}`);
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'clear':
-                    if (guildQueue) {
-                        guildQueue.clearQueue();
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'seek':
-                    if (guildQueue) {
-                        guildQueue.seek(parseInt(args[0] * 1000));
-                    } else {
-                        message.channel.send('No music is playing.');
-                    }
-                    break;
-                case 'playlist':
-                    let playlistqueue = client.player.createQueue(message.guild.id);
-                    await playlistqueue.join(message.member.voice.channel);
-                    song = await playlistqueue.playlist(args.join(' ')).catch(_ => {
-                        if (!guildQueue)
-                            queue.stop();
-                    });
-                    break;
-                case 'radio':
-                    args = ('lofi radio').split(/ +/g);
+            // DEBUG
+            // console.debug(client.guilds.resolve(message.guild.id))
+            // fs.writeFile('./message.debug.log', JSON.stringify(client.guilds.resolve(message.guild.id), null, 2), (err) => {
+            //     if (err) message.channel.send(err);
+            // }
+            // )
+            if (message.content.startsWith(process.env.PREFIX)) {
+                let song;
+                const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+                const command = args.shift().toLowerCase();
+                let guildQueue = client.player.getQueue(message.guild.id);
+                let queue = client.player.getQueue(message.guild.id);
+                if (guildQueue === undefined && queue === undefined) {
                     queue = client.player.createQueue(message.guild.id);
-                    await queue.join(message.member.voice.channel);
-                    song = await queue.play(args.join(' ')).catch(_ => {
-                        if (!guildQueue)
-                            queue.stop();
-                    });
-                    break;
-                default:
-                    message.channel.send('Command not found.');
-                    break;
-            }
+                    guildQueue = queue;
+                }
+                // Pre handling command args
+                switch (command) {
+                    case 'play':
+                    case 'playlist':
+                        if (args.length === 0) {
+                            message.channel.send('Please provide a link or a search query.');
+                            return;
+                        }
+                        break;
+                }
+                switch (command) {
+                    case 'play':
+                        await queue.join(message.member.voice.channel);
+                        song = await queue.play(args.join(' ')).catch(_ => {
+                            if (!guildQueue)
+                                queue.stop();
+                        });
+                        break;
+                    case 'skip':
+                        if (guildQueue) {
+                            guildQueue.skip();
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'stop':
+                        if (guildQueue) {
+                            guildQueue.stop();
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'pause':
+                        if (guildQueue) {
+                            guildQueue.setPaused(true);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'resume':
+                        if (guildQueue) {
+                            guildQueue.setPaused(false);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'volume':
+                        if (guildQueue) {
+                            message.channel.send(`Volume is ${guildQueue.volume}`);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'queue':
+                        if (guildQueue) {
+                            message.channel.send(guildQueue);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'repeat':
+                        if (guildQueue) {
+                            guildQueue.setRepeatMode(1);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'norepeat':
+                        if (guildQueue) {
+                            guildQueue.setRepeatMode(0);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'shuffle':
+                        if (guildQueue) {
+                            guildQueue.Shuffle();
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'queueloop':
+                        if (guildQueue) {
+                            guildQueue.setRepeatMode(2);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'setvolume':
+                        if (guildQueue) {
+                            guildQueue.setVolume(parseInt(args[0]));
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'np':
+                        if (guildQueue) {
+                            message.channel.send(`Now playing: ${guildQueue.nowPlaying}`);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'help':
+                        message.channel.send({ embeds: [helpMessage] });
+                        break;
+                    case 'progress':
+                        if (guildQueue) {
+                            message.channel.send(`Progress: ${guildQueue.createProgressBar().prettier()}`);
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'clear':
+                        if (guildQueue) {
+                            guildQueue.clearQueue();
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'seek':
+                        if (guildQueue) {
+                            guildQueue.seek(parseInt(args[0] * 1000));
+                        } else {
+                            message.channel.send('No music is playing.');
+                        }
+                        break;
+                    case 'playlist':
+                        let playlistqueue = client.player.createQueue(message.guild.id);
+                        await playlistqueue.join(message.member.voice.channel);
+                        song = await playlistqueue.playlist(args.join(' ')).catch(_ => {
+                            if (!guildQueue)
+                                queue.stop();
+                        });
+                        break;
+                    case 'radio':
+                        args = ('lofi radio').split(/ +/g);
+                        queue = client.player.createQueue(message.guild.id);
+                        await queue.join(message.member.voice.channel);
+                        song = await queue.play(args.join(' ')).catch(_ => {
+                            if (!guildQueue)
+                                queue.stop();
+                        });
+                        break;
+                    default:
+                        message.channel.send('Command not found.');
+                        break;
+                }
 
-            // Init event listener for player
-            client.player
-                // Emitted when channel was empty.
-                .on('channelEmpty', (queue) =>
-                    message.channel.send(`Everyone left the Voice Channel, queue ended.`))
-                // Emitted when a song was added to the queue.
-                .on('songAdd', (queue, song) =>
-                    message.channel.send(`Song ${song} was added to the queue.`))
-                // Emitted when a playlist was added to the queue.
-                .on('playlistAdd', (queue, playlist) =>
-                    message.channel.send(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
-                // Emitted when there was no more music to play.
-                .on('queueDestroyed', (queue) =>
-                    message.channel.send(`The queue was destroyed.`))
-                // Emitted when the queue was destroyed (either by ending or stopping).    
-                .on('queueEnd', (queue) =>
-                    message.channel.send(`The queue has ended.`))
-                // Emitted when a song changed.
-                .on('songChanged', (queue, newSong, oldSong) =>
-                    message.channel.send(`${newSong} is now playing.`))
-                // Emitted when a first song in the queue started playing.
-                .on('songFirst', (queue, song) =>
-                    message.channel.send(`Started playing ${song}.`))
-                // Emitted when someone disconnected the bot from the channel.
-                .on('clientDisconnect', (queue) =>
-                    message.channel.send(`I was kicked from the Voice Channel, queue ended.`))
-                // Emitted when deafenOnJoin is true and the bot was undeafened
-                .on('clientUndeafen', (queue) =>
-                    message.channel.send(`I got undefeanded.`))
-                // Emitted when there was an error in runtime
-                .on('error', (error, queue) => {
-                    message.channel.send(`Error: ${error} in ${queue.guild.name}`);
-                });
+                // Init event listener for player
+                client.player
+                    // Emitted when channel was empty.
+                    .on('channelEmpty', (queue) =>
+                        message.channel.send(`Everyone left the Voice Channel, queue ended.`))
+                    // Emitted when a song was added to the queue.
+                    .on('songAdd', (queue, song) =>
+                        message.channel.send(`Song ${song} was added to the queue.`))
+                    // Emitted when a playlist was added to the queue.
+                    .on('playlistAdd', (queue, playlist) =>
+                        message.channel.send(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+                    // Emitted when there was no more music to play.
+                    .on('queueDestroyed', (queue) =>
+                        message.channel.send(`The queue was destroyed.`))
+                    // Emitted when the queue was destroyed (either by ending or stopping).    
+                    .on('queueEnd', (queue) =>
+                        message.channel.send(`The queue has ended.`))
+                    // Emitted when a song changed.
+                    .on('songChanged', (queue, newSong, oldSong) =>
+                        message.channel.send(`${newSong} is now playing.`))
+                    // Emitted when a first song in the queue started playing.
+                    .on('songFirst', (queue, song) =>
+                        message.channel.send(`Started playing ${song}.`))
+                    // Emitted when someone disconnected the bot from the channel.
+                    .on('clientDisconnect', (queue) =>
+                        message.channel.send(`I was kicked from the Voice Channel, queue ended.`))
+                    // Emitted when deafenOnJoin is true and the bot was undeafened
+                    .on('clientUndeafen', (queue) =>
+                        message.channel.send(`I got undefeanded.`))
+                    // Emitted when there was an error in runtime
+                    .on('error', (error, queue) => {
+                        message.channel.send(`Error: ${error} in ${queue.guild.name}`);
+                    });
+            }
+        }
+        catch (err) {
+            console.error(err);
         }
     },
 }
