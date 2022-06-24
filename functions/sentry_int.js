839a465992f8924fefddb80add4f1a68a161ce6a
lekiet1214 <sisu1214@gmail.com>
2022-06-24 19:12:15 +0700

    sentry

--- functions/sentry_int.js
+++ functions/sentry_int.js
@@ -1,24 +1,12 @@
-const Sentry = require("@sentry/node");
 // or use es6 import statements
 // import * as Sentry from '@sentry/node';
 
-const Tracing = require("@sentry/tracing");
 // or use es6 import statements
 // import * as Tracing from '@sentry/tracing';
 
-Sentry.init({
-  dsn: "https://e6862534b6ec499d9524da05bd40ee5e@o1298047.ingest.sentry.io/6527738",
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
-});
+
 
 setTimeout(() => {
   try {
