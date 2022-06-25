55195e701b4f46d421e4f62fdf11afd80798d400
lekiet1214 <sisu1214@gmail.com>
2022-06-25 19:51:53 +0700

    typoo

--- bot.js
+++ bot.js
@@ -28,7 +28,8 @@ client.distube = new DisTube(client, {
     new SoundCloudPlugin(),
     new YtDlpPlugin()
   ],
-  youtubeDL: false
+  youtubeDL: false,
+  searchSongs: 5
 })
 client.commands = new Discord.Collection()
 client.aliases = new Discord.Collection()
@@ -70,14 +71,12 @@ client.on('messageCreate', async message => {
 })
 
 const status = queue =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ') || 'Off'}\` | Loop: \`${
-    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
   }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
 client.distube
   .on('playSong', (queue, song) =>
     queue.textChannel.send(
-      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
-        song.user
+      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user
       }\n${status(queue)}`
     )
   )
@@ -88,8 +87,7 @@ client.distube
   )
   .on('addList', (queue, playlist) =>
     queue.textChannel.send(
-      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
+      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${playlist.songs.length
       } songs) to queue\n${status(queue)}`
     )
   )
@@ -102,21 +100,20 @@ client.distube
     message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
   )
   .on('finish', queue => queue.textChannel.send('Finished!'))
-// // DisTubeOptions.searchSongs = true
-// .on("searchResult", (message, result) => {
-//     let i = 0
-//     message.channel.send(
-//         `**Choose an option from below**\n${result
-//             .map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``)
-//             .join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`
-//     )
-// })
-// .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`))
-// .on("searchInvalidAnswer", message =>
-//     message.channel.send(
-//         `${client.emotes.error} | Invalid answer! You have to enter the number in the range of the results`
-//     )
-// )
-// .on("searchDone", () => {})
+  .on('searchResult', (message, result) => {
+    let i = 0
+    message.channel.send(
+      `**Choose an option from below**\n${result
+        .map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``)
+        .join('\n')}\n*Enter anything else or wait 60 seconds to cancel*`
+    )
+  })
+  .on('searchCancel', message => message.channel.send(`${client.emotes.error} | Searching canceled`))
+  .on('searchInvalidAnswer', message =>
+    message.channel.send(
+      `${client.emotes.error} | Invalid answer! You have to enter the number in the range of the results`
+    )
+  )
+  .on('searchDone', () => { })
 
-client.login(process.env.token)
+client.login(process.env.TOKEN)
