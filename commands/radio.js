67b5c5bec3af01540eab5c7657d67c019eec4a50
lekiet1214 <sisu1214@gmail.com>
2022-06-27 08:28:27 +0700

    radio command

---
+++ commands/radio.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'radio',
+  run: async (client, message, args) => {
+    const string = 'https://www.youtube.com/watch?v=tFFy0yEYki0'
+    const searchResult = await client.distube.search(string)
+    if (!searchResult) return message.channel.send(`${client.emotes.error} | Something went wrong, please try again!.`)
+    client.distube.play(message.member.voice.channel, searchResult.url, {
+      member: message.member,
+      textChannel: message.channel,
+      message
+    })
+  }
+}
