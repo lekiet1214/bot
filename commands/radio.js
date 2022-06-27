10dcd4ffff4ec4fd3c692957bdd17ad9c0f4958b
lekiet1214 <sisu1214@gmail.com>
2022-06-27 08:33:37 +0700

    uptime format

--- commands/radio.js
+++ commands/radio.js
@@ -4,7 +4,7 @@ module.exports = {
     const string = 'https://www.youtube.com/watch?v=tFFy0yEYki0'
     const searchResult = await client.distube.search(string)
     if (!searchResult) return message.channel.send(`${client.emotes.error} | Something went wrong, please try again!.`)
-    client.distube.play(message.member.voice.channel, searchResult.url, {
+    client.distube.play(message.member.voice.channel, searchResult[0], {
       member: message.member,
       textChannel: message.channel,
       message
