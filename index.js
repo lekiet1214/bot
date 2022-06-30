68d40b72eeed51eb887c81530af3dc9e61ff31d1
lekiet1214 <sisu1214@gmail.com>
2022-06-30 10:23:29 +0700

    data request

--- index.js
+++ index.js
@@ -6,23 +6,16 @@ const manager = new ShardingManager('./bot.js', {
   token: bottoken
 })
 const logdna = require('@logdna/logger')
-
+const fs = require('fs')
 const options = {
   app: 'github',
   level: 'info' // set a default for when level is not provided in function calls
 }
 
 const logger = logdna.createLogger('103432aaec3b31a9af650c41daa2bff9', options)
-
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = `mongodb+srv://nhobot:${process.env.MONGODB_PASSWORD}@discord0.ve7qj.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-client.connect(err => {
-  const collection = client.db('test').collection('devices')
-  if (err) logger.error('MongoDB error: ', err, '\n', collection)
-  // perform actions on the collection object
-  client.close()
-})
 
 manager
   .spawn()
@@ -43,3 +36,23 @@ manager.on('shardCreate', shard => {
     shard.send({ type: 'shardId', data: { shardId: shard.id } })
   })
 })
+
+async function getData () {
+  try {
+    await client.connect()
+
+    // Get the collection
+    const database = client.db('guild')
+    const guilddb = database.collection('settings')
+    fs.writeFile('./data/guildSettings.json', JSON.stringify(await guilddb.find().toArray()), 'utf-8', err => {
+      if (err) console.error(err)
+    }
+    )
+    // logger.log('Guild data saved.')
+  } finally {
+    console.log('Guild data saved.')
+    client.close()
+  }
+}
+
+getData().catch(console.error)
